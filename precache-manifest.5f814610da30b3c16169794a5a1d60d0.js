71a369d1aae8ecb2e54d71b68ddeef3366c6b308
Xero321 <etemuujin789@gmail.com>
2020-06-12 00:56:52 +0900

    Updates

---
+++ precache-manifest.5f814610da30b3c16169794a5a1d60d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "db5a1555089e5a006ebd",
+    "url": "/hero/static/css/main.d5de6eb9.chunk.css"
+  },
+  {
+    "revision": "db5a1555089e5a006ebd",
+    "url": "/hero/static/js/main.db5a1555.chunk.js"
+  },
+  {
+    "revision": "e5b0dd6e3146b744919c",
+    "url": "/hero/static/js/runtime~main.e5b0dd6e.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/hero/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "89ec19d911d51bcf8cd15f0042e47776",
+    "url": "/hero/index.html"
+  }
+];
